ac68466f6b6837a1b697bdf69178add5e213b5fb
andreeto <tony.andreev94@outlook.com>
2020-01-21 18:04:07 +0200

    # Some slight changes added.

--- beta_log_analyzer_2.0.py
+++ beta_log_analyzer_2.0.py
@@ -1,5 +1,10 @@
 import os
 import re
+import time
+
+start_time = time.time()
+
+# [24;1H[2K[24;1H[1;24r[24;1H
 
 username = os.getlogin()
 # TO_DO: add filename via user input
@@ -10,6 +15,7 @@ critical_errors_list = ["Other Fault", "Fan failure", "MM1  Failed", "MM2  Faile
                         "Unrecoverable fault on PoE controller", "PD Other Fault"]
 
 warning = ""
+second_warning = ""
 warning_list = []
 # test_list for warning_list testing
 test_list = []
@@ -29,8 +35,12 @@ print(f"Critical errors found in logfile:")
 print("#################################")
 print()
 
+
 with open(file_path) as file_object:
     for line in file_object:
+        # Breaking out of the file so we don't check the whole file unnecessarily
+        if 'Bottom of Log :' in line:
+            break
         for error_element in critical_errors_list:
             if error_element in line:
                 errors_counter += 1
@@ -52,8 +62,14 @@ with open(file_path) as file_object:
             product_number_regex = re.search(r"[J]\w\d{3}\w", product_number)
         # TO_DO: add these two IFs into a single if - using or makes every line to get appended in the list
         # TO_DO: add major errors: M 03/07/18 08:48:32 02796 chassis: AM1: Internal power supply 1 inserted. Total
-        if 'W 0' in line:
-            warning = line
+        if 'W 0' in previous_line:
+            # Deal with 1- or 2-line warnings with doing the check in "previous_line"
+            # use regex to check if "line" starts with whitespace - print it as well.
+            warning = previous_line
+            warning_list.append(warning)
+            if re.search(r"[\s]", line):
+                second_warning = line
+                warning_list.append(second_warning)
             warning_list.append(warning)
         if 'W 1' in line:
             warning = line
@@ -69,6 +85,7 @@ else:
     print('Possible replacement needed.')
     print()
 
+print()
 print("#################################")
 print(f"USEFUL SWITCH INFORMATION: ")
 print("#################################")
@@ -78,6 +95,7 @@ print(f"Software revision: {software_rev_regex.group()}")
 print(f"Uptime: {up_time_regex.group()}")
 print(f"Product number: {product_number_regex.group()}")
 print()
+print()
 
 
 print("#################################")
@@ -86,8 +104,13 @@ print("#################################")
 print(f"http://partsurfer.hpe.com/Search.aspx?SearchText={product_number_regex.group()}")
 print(f"https://h10145.www1.hpe.com/downloads/SoftwareReleases.aspx?ProductNumber={product_number_regex.group()}")
 print()
+print()
+print("#################################")
 print(f"Warnings found:")
+print("#################################")
 # TO_DO: remove empty line between each warning, fix issue with warnings on two lines
-print(*warning_list, sep = "\n")
+print(*warning_list)
+
+print("--- %s seconds ---" % (time.time() - start_time))
 #print(warning_list[:])
 #print(test_list[:])
