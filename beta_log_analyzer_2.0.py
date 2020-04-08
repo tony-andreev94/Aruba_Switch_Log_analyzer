7f26b16beb8536b5da064fa5c08119cab3d08caf
andreeto <tony.andreev94@outlook.com>
2020-04-08 21:44:46 +0300

    # The logfile is loaded, formatted and analyzed successfully. There is an issue with Up time and Results class needs work.

--- beta_log_analyzer_2.0.py
+++ beta_log_analyzer_2.0.py
@@ -1,4 +1,4 @@
-# TODO: Add a general description of the program?
+# TODO: Add a general description of the program? Good Practices?!
 import os
 import re
 import time
@@ -7,31 +7,22 @@ start_time = time.time()
 # Setting up a variable that gets the user's username
 username = os.getlogin()
 # TODO: add filename or file path via user input?
-file_path = 'C:\\Users\\'+username+'\\Documents\\@Python\\sta.txt'
+file_path = 'C:\\Users\\' + username + '\\Documents\\@Python\\sta.txt'
 
 # A list with critical error messages to be searched in the logs
-critical_errors_list = ["Other Fault", "Fan failure", "MM1  Failed", "MM2  Failed", "Faulted", "PD Other Fault"
+critical_errors_list = ["Other Fault", "Fan failure", "MM1  Failed", "MM2  Failed", "Faulted", "PD Other Fault",
                         "Unrecoverable fault on PoE controller", "Failures", "selftest failure"]
 
-# Variables and a list used to get and store warning messages
-# the "second_warning" variable is used to get the second part of two-line error messages
-warning = ""
-second_warning = ""
-warning_list = []
-# Variable used to hold the product number of the device, used in "USEFUL LINKS" and "USEFUL INFORMATION" sections
-product_number = ""
+warning = ""  # variable to store found warning
+second_warning = ""  # used to catch two line warnings
+warnings_found = []  # stores warnings
+criticals_found = []  # stores critical errors
+product_n_line = ""
 errors_counter = 0
-# Variable storing the previously read line of the logfile
-previous_line = ""
-log_time = str
-software_rev = str
-up_time = str
-
-print("#################################")
-print(f"Critical errors found in logfile:")
-print("#################################")
-print()
-
+previous_line = ""  # Variable storing the previously read line of the logfile
+log_time = ""
+sw_rev_line = ""
+up_time_line = ""
 
 # TODO Use another loop and .rstrip() to remove all blank lines from the file
 with open(file_path) as file_object:
@@ -42,29 +33,26 @@ with open(file_path) as file_object:
         # ~1300 lines of logs are read instead of ~20000
         if 'Bottom of Log :' in line:
             break
-        # Another loop checking if an element from the error list is present in the currently read line
-        for error_element in critical_errors_list:
+        for error_element in critical_errors_list:  # find and store critical errors
             if error_element in line:
                 errors_counter += 1
-                print(line.rstrip())
-        # warning_regex = re.search(r"[W]\s\d{2}[/]\d{2}[/]\d{2}", line)
+                criticals_found.append(line)
 
         # Finding useful information about the device - not related to errors or warnings
-        # TODO: Possibly put this info at the top? Ask the user if this info is to be included?
         if 'Up Time' in line:
             # If the desired string is found in the line we set the variable's value to the line of the log
-            up_time = line
+            up_time_line = line
             # REGEX is used to extract only the meaningful information from the line of text
-            up_time_regex = re.search(r"\d+\s\w+", up_time)
+            up_time = re.search(r"\d+\s\w+", up_time_line)
         if 'Software revision' in line:
-            software_rev = line
-            software_rev_regex = re.search(r"\w{2}[.]\w+[.]\w+[.]\w+", software_rev)
+            sw_rev_line = line
+            software_rev = re.search(r"\w{2}[.]\w+[.]\w+[.]\w+", sw_rev_line)
         if 'show time' in previous_line:
             log_time = line
         # Gets the product number of the device - also needed for the "USEFUL LINKS" section
         if 'Product:   HP J' in line:
-            product_number = line
-            product_number_regex = re.search(r"[J]\w\d{3}\w", product_number)
+            product_n_line = line
+            product_number = re.search(r"[J]\w\d{3}\w", product_n_line)
 
         # Example warning/major messages:
         # W 10/25/13 17:42:51 00374 chassis: WARNING: SSC is out of Date: Load 8.2 or
@@ -77,22 +65,14 @@ with open(file_path) as file_object:
         # The second if looks if the next message starts with whitespace
         if re.search(r"[W]\s\d", previous_line):
             warning = previous_line
-            warning_list.append(warning)
+            warnings_found.append(warning)
             if re.search(r"[\s]", line):
                 second_warning = line
-                warning_list.append(second_warning)
+                warnings_found.append(second_warning)
 
         # At the end of the loop we use the previous_line variable to store the line of text before the next line of log
         # is checked.
         previous_line = line
-
-# Print the message if critical errors are found in the logfile.
-if errors_counter == 0:
-    print('No critical errors were found in the logfile.')
-else:
-    print()
-    print('Possible replacement needed.')
-    print()
 
 print()
 # Print useful device information
@@ -101,9 +81,9 @@ print(f"USEFUL SWITCH INFORMATION: ")
 print("#################################")
 print()
 print(f"Log generated on: {log_time.rstrip()}")
-print(f"Software revision: {software_rev_regex.group()}")
-print(f"Up time: {up_time_regex.group()}")
-print(f"Product number: {product_number_regex.group()}")
+print(f"Software revision: {software_rev.group()}")
+print(f"Up time: {up_time.group()}")
+print(f"Product number: {product_number.group()}")
 print()
 print()
 
@@ -112,16 +92,31 @@ print("#################################")
 print(f"USEFUL LINKS: ")
 print("#################################")
 print("Order a replacement part:")
-print(f"http://partsurfer.hpe.com/Search.aspx?SearchText={product_number_regex.group()}")
+print(f"http://partsurfer.hpe.com/Search.aspx?SearchText={product_number.group()}")
 print("Download the latest software:")
-print(f"https://h10145.www1.hpe.com/downloads/SoftwareReleases.aspx?ProductNumber={product_number_regex.group()}")
+print(f"https://h10145.www1.hpe.com/downloads/SoftwareReleases.aspx?ProductNumber={product_number.group()}")
 print()
 print()
+# Print the message if critical errors are found in the logfile.
+print("#################################")
+print(f"Critical errors found in logfile:")
+print("#################################")
+print()
+if errors_counter == 0:
+    print('No critical errors were found in the logfile.')
+else:
+    print(*criticals_found)
+    print()
+    print('Possible replacement needed.')
+    print()
+
 # Print the list of warnings
 print("#################################")
 print(f"Warnings found:")
 print("#################################")
-print(*warning_list)
+print(*warnings_found)
 
 # Printing time taken to analyze the log (for testing purposes)
-print("--- %s seconds ---" % (time.time() - start_time))
+# print("--- %s seconds ---" % (time.time() - start_time))
+print(f"Time taken: {time.time() - start_time:.3f} sec.")
+
